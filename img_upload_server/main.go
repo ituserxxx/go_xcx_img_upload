1a7272ae89e58c712777d25164727c4c0f2deeec
jushitangguoshan <1034610091@qq.com>
2021-12-23 11:30:18 +0800

    sd

--- img_upload_server/main.go
+++ img_upload_server/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"github.com/gogf/gf/frame/g"
 	_ "img-xcx-tool/boot"
 	_ "img-xcx-tool/router"
-
-	"github.com/gogf/gf/frame/g"
 )
 
 func main() {
 	g.Server().Run()
+//https://developers.weixin.qq.com/doc/offiaccount/Asset_Management/Adding_Permanent_Assets.html
+
 }
